ca5546eb03217355f6a589c5de5639ca702c1820
pmathew96 <paul.mathew@ashoka.edu.in>
2018-04-02 02:50:26 +0530

    Finished program

--- main.js
+++ main.js
@@ -1,77 +1,49 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicG1hdGhldyIsImEiOiJjamR5aXJmeDIwNHQzMnFzMWxzOTdubTloIn0.eAYLsNu4Kd2UbLU9fLcuDw';
+// define access token
+mapboxgl.accessToken = 'pk.eyJ1IjoicG1hdGhldyIsImEiOiJjamZndjE5YnEzaGNzMnFtbWV1YzJxd2h2In0.xNYB4P4ljTh4ogGvlGNJ0A';
 
-var chapters = {
-    'part-1': {
-        center: [82.8, 23.88],
-        zoom: 3.5,
-        bearing: 0,
-        pitch: 0,
-    },
-    'part-2': {
-        center: [96.420066, 28.365358],
-        zoom: 9,
-        bearing: 0,
-        pitch: 52,
-    },
-    'part-3': {
-        center: [86.586872, 26.873760],
-        zoom: 9,
-        bearing: 0,
-        pitch: 52,
-    },
-    'part-4': {
-        center: [95.659549, 24.003677],
-        zoom: 9,
-        bearing: 0,
-        pitch: 52,
-    }
-};
-
-var map;
-
-window.onload = function() {
-    map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/pmathew/cjff56k8rb8mu2rs55ycs27f2',
-        center: [82.8, 23.88],
-        zoom: 4,
-        bearing: 0,
-        pitch: 0,
-    });
-
-
-map.on('click', 'earthquakes', function(e){
-    var place = e.features[0].properties.place;
-    console.log(place)
-    if (place == "eastern Xizang-India border region"){
-        setActiveChapter('part-2');
-        var top = document.getElementById('part-2').offsetTop; //Getting Y of target element
-        window.scrollTo(0, top+5);         
-    }
-    
-    if (place == "Nepal-India border region"){
-        setActiveChapter('part-3');
-        var top = document.getElementById('part-3').offsetTop; //Getting Y of target element
-        window.scrollTo(0, top);         
-    }
-    if (place == "Myanmar"){
-        setActiveChapter('part-4');
-        var top = document.getElementById('part-4').offsetTop; //Getting Y of target element
-        window.scrollTo(0, top);         
-    }
+//create map
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/pmathew/cjff56k8rb8mu2rs55ycs27f2' // map style URL from Mapbox Studio
 });
-    
-var popup = new mapboxgl.Popup({
+
+// wait for map to load before adjusting it
+map.on('load', function() {
+
+    // make a pointer cursor
+    map.getCanvas().style.cursor = 'default';
+
+  
+    // define layer names
+    var layers = ['0-0.5', '0.5-0.55', '0.55-0.6', '0.6-0.65', '0.65-0.7', '0.7-0.75'];
+    var colors = ['#ffffcc', '#d9f0a3', '#addd8e', '#78c679', '#31a354', '#006837'];
+
+    // create legend
+    for (i=0; i<layers.length; i++) {
+        var layer = layers[i];
+        var color = colors[i];
+        var item = document.createElement('div');
+        var key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
+
+        var value = document.createElement('span');
+        value.innerHTML = layer;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
+    }
+
+    var popup = new mapboxgl.Popup({
         closeButton: false,
         closeOnClick: true
     });
 
-    map.on('mouseenter', 'earthquakes', function(e) {
+    map.on('click', 'india-states-hdi', function(e) {
         // Change the cursor style as a UI indicator.
         map.getCanvas().style.cursor = 'pointer';
 
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var description = e.features[0].properties.description;
+        var coordinates = e.features[0].geometry.coordinates;
 
         // Ensure that if the map is zoomed out such that multiple
         // copies of the feature are visible, the popup appears
@@ -83,7 +55,7 @@ var popup = new mapboxgl.Popup({
         // Populate the popup and set its coordinates
         // based on the feature found.
         popup.setLngLat(coordinates)
-            .setHTML('<h3>' + e.features[0].properties.place + '</h3><p>Magnitude: ' + e.features[0].properties.mag + '</p>')
+            .setHTML('<h3>' + e.features[0].properties.NAME_1 + '</h3><p>HDI: ' + e.features[0].properties.HDI + '</p>')
             .addTo(map);
     });
 
@@ -92,37 +64,4 @@ var popup = new mapboxgl.Popup({
         popup.remove();
     });
 
-}
-
-// On every scroll event, check which element is on screen
-window.onscroll = function() {
-    var chapterNames = Object.keys(chapters);
-    console.log(chapterNames);
-    for (var i = 0; i < chapterNames.length; i++) {
-        var chapterName = chapterNames[i];
-        if (isElementOnScreen(chapterName)) {
-            setActiveChapter(chapterName);
-            break;
-        }
-    }
-};
-
-
-var activeChapterName = 'part-1';
-function setActiveChapter(chapterName) {
-    if (chapterName === activeChapterName) return;
-
-    map.flyTo(chapters[chapterName]);
-
-    document.getElementById(chapterName).setAttribute('class', 'active');
-    document.getElementById(activeChapterName).setAttribute('class', '');
-
-    activeChapterName = chapterName;
-}
-
-function isElementOnScreen(id) {
-    var element = document.getElementById(id);
-    var bounds = element.getBoundingClientRect();
-    return bounds.top < window.innerHeight && bounds.bottom > 0;
-}
-
+});
